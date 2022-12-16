601a8f4b04553d8bee6bd478546dd53dcb4cbd73
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-15 20:10:08 -0500

    working on bonus now

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "../lib/user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(string)
+        @string = string
+        @knowledge << string
+    end
+
+
 
 end
